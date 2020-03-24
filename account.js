41397431fed076d94e5b74ab8a0475e5cee87011
Felipe Bohorquez <fbohorqu@hotmail.com>
2020-03-24 17:48:04 +0000

    Automatically backed up by Learn

--- account.js
+++ account.js
@@ -16,8 +16,8 @@ function account() {
     }
   }
 
-  var signupBonus = 250,
-  deposit = 1000
+  var signupBonus = 250;
+  var deposit = 1000;
 
   var openAccount = function(account, deposit) {
     account.balance += signupBonus
